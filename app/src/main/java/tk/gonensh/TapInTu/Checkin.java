c28d935516cf975b594d1829a2727627325ccbe9
Gonen Shoham <gonensh@gmail.com>
2015-04-19 05:37:37 -0400

    Added User,Checkin Classes and user flow functions

--- app/src/main/java/tk/gonensh/TapInTu/Checkin.java
+++ app/src/main/java/tk/gonensh/TapInTu/Checkin.java
@@ -3,30 +3,19 @@ package tk.gonensh.TapInTu;
 /**
  * Created by gonen on 4/19/15.
  */
-/*public class Checkin {
-    Long id;
-    String timestamp;
-    Long userId;
 
-    public Checkin(Long userId){
-        this.userId = userId;
-        this.timestamp = new Date().toString();
-        this.id = UUID.randomUUID().getLeastSignificantBits();
-    }
-}
-*/
 class Checkin {
     private long userId;
-    private String fullName;
+    private String timestamp;
     public Checkin() {}
-    public Checkin(String fullName, long userId) {
-        this.fullName = fullName;
+    public Checkin(String timestamp, long userId) {
+        this.timestamp = timestamp;
         this.userId = userId;
     }
     public long getUserId() {
         return userId;
     }
-    public String getFullName() {
-        return fullName;
+    public String getTimestamp() {
+        return timestamp;
     }
 }
